914a749dcc8a76887841f1b9778effff031947ec
Cat <catholm1@outlook.com>
2022-11-01 20:10:13 +0100

    style change

--- index.js
+++ index.js
@@ -3,10 +3,13 @@
     //Math.random() * 10 Return a random number between 0 and 10:
 let num1 = Math.ceil(Math.random() * 10)
 let num2 = Math.ceil(Math.random() * 10)
-let calculation = document.getElementsByClassName("btn").value
+
 
 //getting the question element
 const questionEl = document.getElementById("question");
+
+ //getting the input element by id with the value
+ let inputEl = document.getElementById("input");
 
 
 //getting the score element by id
@@ -25,43 +28,52 @@ scoreEl.innerText = `${score}`;
 
 function add() {
 
-    //getting the input element by id with the value
-    let inputEl = document.getElementById("input").value;
-
+    console.log('button is clicked')
     let correctAns = num1 + num2
+
+    document.getElementById("sum-el").textContent = correctAns;
 
 }
 
 function subtract() {
 
-    //getting the input element by id with the value
-    let inputEl = document.getElementById("input").value;
-
+    console.log('button is clicked')
     let correctAns = num1 - num2
+
+    document.getElementById("sum-el").textContent = correctAns;
 
 }
 
 function divide() {
-    //getting the input element by id with the value
-    let inputEl = document.getElementById("input").value;
-
+   
+    console.log('button is clicked')
     let correctAns = num1 / num2
+
+    document.getElementById("sum-el").textContent = correctAns;
 
 }
 
 function multiply() {
 
-        //getting the input element by id with the value
-        let inputEl = document.getElementById("input").value;
+    console.log('button is clicked')
 
         //making the correct answer 
         let correctAns = num1 * num2;
         
-        // If inputValue is Not a Number or less than one or greater than 10
-        let text;
-  
-        // If inputValue is Not a Number or not equal to correctAns
-        if (isNaN(inputEl) || inputEl != correctAns) {
+        document.getElementById("sum-el").textContent = correctAns;
+
+  } 
+
+function userAns() {
+
+     //getting the input value
+     const userAns = inputEl.value;
+
+     // If inputValue is Not a Number or less than one or greater than 10
+     let text;
+
+    // If inputValue is Not a Number or not equal to correctAns
+    if (isNaN(inputEl) || userAns != correctAns) {
                 
             text = "Input is wrong";
             document.getElementById("text").style.color = "red";
@@ -69,22 +81,20 @@ function multiply() {
             updateLocalStorage()
 
             console.log(score);
-        } else {
+    } else {
             text =  " correct !";
             document.getElementById("text").style.color = "green";
             score = score + 1;
             updateLocalStorage()
 
             console.log(score);
-        }
+    }
         
         document.getElementById("text").textContent = `${text}`;
         document.getElementById("score").textContent = `score: ${score}`;
         document.getElementById("sum-el").textContent = correctAns;
-
         updateLocalStorage()
-  } 
-
+}
 
 function updateLocalStorage() {
     localStorage.setItem("score", JSON.stringify(score));
